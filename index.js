0e87566ea9676d12db964086a998f3714a92b751
Kai Shovlin <kai.k.b.shovlin@gmail.com>
2019-01-07 13:37:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,11 +14,14 @@ function logWhisper(string) {
   console.log('hello')
 }
 
-function sayHiToGrandma(string) {
-  
-  var lowercase = 'hello!'
-
-lowercase.toLowerCase() === lowercase // true
-
-return "I can't hear you!"
+function sayHiToGrandma(string){
+    if (string.toLowerCase() === string){
+      return 'I can\'t hear you!';
+    }
+    if (string.toUpperCase() === string){
+      return 'YES INDEED!';
+    }
+    if (string === 'I love you, Grandma.'){
+      return 'I love you, too.';
+    }
 }
